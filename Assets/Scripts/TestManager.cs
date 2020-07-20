88506fb789b2e42e5ce65f44991e365a2a0a82ff
Paul Redetzky <p_redetzky@gmx.de>
2020-07-20 12:26:22 +0200

    blorgh

---
+++ Assets/Scripts/TestManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEditor;
+
+public class TestManager : MonoBehaviour
+{
+    [SerializeField] Song song;
+    [SerializeField] AudioSource audioSource;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
