e2893e1472c7a05f9cffa8aa9b9c98c301ae4060
Paul Redetzky <p_redetzky@gmx.de>
2020-07-26 23:29:44 +0200

    added chairs & first ui

---
+++ Assets/Scripts/BeatBarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+public class BeatBarController : MonoBehaviour
+{
+    [SerializeField] RectTransform one, two, three, four;
+    [SerializeField] AnimationCurve beatProgression;
+    [SerializeField] RectTransform circle;
+     RectTransform[] beats;
+
+    public int currBeat;
+    public double timeToNextBeat;
+    public double timeOnCurrentBeat;
+    private void Update()
+    {
+        float animProg = beatProgression.Evaluate((float)(timeOnCurrentBeat / timeToNextBeat));
+        circle.position = Vector3.Lerp(
+            beats[currBeat].position,
+            beats[(currBeat + 1) % beats.Length].position,
+            animProg);
+    }
+
+    private void Start()
+    {
+        beats = new RectTransform[]
+        {
+            one,
+            two,
+            three,
+            four
+        };
+        timeToNextBeat = 1.0f;
+
+    }
+
+
+}
--- Assets/Scripts/TestManager.cs
+++ Assets/Scripts/TestManager.cs
@@ -3,18 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEditor;
+using System;
 
 public class TestManager : MonoBehaviour
 {
     [SerializeField] Song song;
     [SerializeField] AudioSource audioSource;
 
-
-
-
-    // Update is called once per frame
-    void Update()
+    BeatBarController beatBarController;
+    int currBeat;
+    int totalBeats;
+    bool songRunning;
+    double startDSPTime;
+    double currBeatStartTime;
+    double lastDSPTime;
+    double dspDelta;
+    double timeOnCurrentBeat;
+    private void Awake()
     {
-        
+        songRunning = false;
+        beatBarController = FindObjectOfType<BeatBarController>();
+        dspDelta = 0;
+        lastDSPTime = 0;
+        startDSPTime = 0;
+        currBeatStartTime = 0;
+        currBeat = 0;
+        totalBeats = 0;
+    }
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            StartSong();
+        if (songRunning)
+            UpdateRunning();
+    }
+
+    private void UpdateRunning()
+    {
+        double dspTime = AudioSettings.dspTime;
+        double dspDiff = song.beats[totalBeats].dspTime - dspTime;
+        timeOnCurrentBeat += dspDelta;
+
+        if (dspDiff <= 0)
+        {
+            NextBeat(dspTime, dspDiff);
+        }
+        beatBarController.timeOnCurrentBeat = timeOnCurrentBeat;
+
+        lastDSPTime = dspTime;
+    }
+
+    private void NextBeat(double dspTime, double dspDiff)
+    {
+        totalBeats = Math.Min(totalBeats + 1, song.beats.Count - 1);
+        currBeat = totalBeats % 4; // because song is in 4/4
+        currBeatStartTime = dspTime;
+        double relativeBeatLength = song.beats[currBeat + 1].dspTime - song.beats[currBeat].dspTime;
+        beatBarController.timeToNextBeat = relativeBeatLength;
+        beatBarController.currBeat = currBeat;
+        timeOnCurrentBeat = 0;
+
+    }
+
+    void StartSong()
+    {
+        audioSource.clip = song.audioClip;
+        audioSource.Play();
+        startDSPTime = AudioSettings.dspTime;
+        currBeat = 0;
+        totalBeats = 0;
+        songRunning = true;
     }
 }
